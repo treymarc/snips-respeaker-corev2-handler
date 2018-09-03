fbe8e5c2b40c2092384131224281d9f4abd283d0
Marc Trey <trey.marc@gmail.com>
2018-09-04 01:52:55 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,5 @@
 
-exit
-
-sudo apt-get install python-mraa libmraa1
+apt-get install python-mraa libmraa1
 pip install pixel-ring
 pip install paho-mqtt
 pip install requests
@@ -16,4 +14,4 @@ systemctl enable snips-handler-ww.service
 systemctl start snips-handler-weather.service
 systemctl start snips-handler-ww.service
 
-#ref https://github.com/respeaker/respeaker_v2_eval
+
